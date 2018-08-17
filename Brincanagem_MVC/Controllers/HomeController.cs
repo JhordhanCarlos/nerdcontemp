c1de04c3ae05a4c98b1e4caa390396dbad48cc6d
Jhordhan Carlos <jhordhanjf@gmail.com>
2018-08-17 18:37:55 -0300

    nerdcontemp
    Arquivos do nerdcontemp

---
+++ Brincanagem_MVC/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Brincanagem_MVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Aqui você vai encontrar as opiniões do autor do site sobre os filmes da DC.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Aqui você vai encontrar as opiniões do autor do site sobre os filmes da Marvel.";
+
+            return View();
+        }
+    }
+}
---
+++ Brincanagem_MVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Brincanagem_MVC.MvcApplication" Language="C#" %>
